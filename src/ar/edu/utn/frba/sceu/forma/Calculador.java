8429864155a29017f6cd871ceadaf730383c1e49
Martin Lennard <mlennard.utn@gmail.com>
2019-09-30 22:05:09 -0300

    clase4

--- src/ar/edu/utn/frba/sceu/forma/Calculador.java
+++ src/ar/edu/utn/frba/sceu/forma/Calculador.java
@@ -19,7 +19,18 @@ public class Calculador {
 		
 		System.out.println("El area es: " + forma2.area());
 		System.out.println("El resultado es: " + forma2.esRectangulo());
-
+		
+		int area1 = forma1.area();
+		int area2 = forma2.area();
+		
+		if(area1 > area2) {
+			System.out.println("Forma 1 es mas grande");
+		} else if (area2 > area1) {
+			System.out.println("Forma 2 es mas grande");
+		} else {
+			System.out.println("Las formas son iguales");
+			
+		}
 	}
 	
 	
